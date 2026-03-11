f0150bb8bc0dd53e71b0e00214fbc77e591ddf3f
MAHMADzubair <welcomeahmad5@gmail.com>
2026-03-11 12:38:00 +0500

    vercel

--- Day12/frontend/src/services/api.js
+++ Day12/frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
-
+baseURL: import.meta.env.VITE_API_URL;
 const API = axios.create({
-  baseURL: "/api",
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:3000/api",
 });
 
 // REQUEST INTERCEPTOR
@@ -16,7 +16,6 @@ API.interceptors.request.use(
 
     return config;
   },
-
   (error) => {
     return Promise.reject(error);
   },
