7f55a600e10d2b9a95d650069f6ff9d0737f85bd
MAHMADzubair <welcomeahmad5@gmail.com>
2026-03-11 05:46:31 +0500

    vercel

--- Day11A/Frontend/app.js
+++ Day11A/Frontend/app.js
@@ -1,4 +1,4 @@
-const API = "http://localhost:3000";
+const API = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1' ? "http://localhost:3000" : window.location.origin;
 
 let token = "";
 
